ae26e4c8983421f68bfb6ead057adbaa0865da1e
kihwankim <clack2933@gmail.com>
2021-09-08 20:04:52 +0900

    feat: timezone 설정

--- module-api/src/main/java/com/yapp/sharefood/SharefoodApplication.java
+++ module-api/src/main/java/com/yapp/sharefood/SharefoodApplication.java
@@ -3,8 +3,16 @@ package com.yapp.sharefood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SharefoodApplication {
+    @PostConstruct
+    public void initTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SharefoodApplication.class, args);
     }
