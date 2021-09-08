e483e78265d3b068a1768ef8d206f76f312f5754
kihwankim <clack2933@gmail.com>
2021-09-08 20:09:38 +0900

    feat: application swagger 설정

--- module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
+++ module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
@@ -1,14 +1,16 @@
 package com.yapp.sharefood.healthcheck.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+    @ApiOperation("Application 생존 검사")
     @GetMapping("/healthcheck")
     public ResponseEntity<String> getHealthCheck() {
-        
+
         return ResponseEntity.ok("health check");
     }
 }
