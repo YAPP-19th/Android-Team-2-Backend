ea92beb9648810597cd117c73be1f0a8a8d420ba
kihwankim <clack2933@gmail.com>
2021-09-08 19:12:27 +0900

    feat: 프로젝트 초기 설정

---
+++ module-api/src/main/java/com/yapp/sharefood/SharefoodApplication.java
@@ -0,0 +1,11 @@
+package com.yapp.sharefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharefoodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SharefoodApplication.class, args);
+    }
+}
---
+++ module-common/src/test/java/com/yapp/sharefood/SharefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.sharefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharefoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
