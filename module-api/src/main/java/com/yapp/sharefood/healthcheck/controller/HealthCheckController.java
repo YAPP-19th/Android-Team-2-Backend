5c8e329ce5f3e4d0f0fb8a928efbc0dd5396c24c
kihwankim <clack2933@gmail.com>
2021-09-08 20:05:45 +0900

    refactor: controller 위치 이동

--- module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
+++ module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.yapp.sharefood;
+package com.yapp.sharefood.healthcheck.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
