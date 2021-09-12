5176096890d66c47090a33f236513822cde8cdcb
kihwankim <clack2933@gmail.com>
2021-09-12 20:10:59 +0900

    feat: host port 확인

--- module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
+++ module-api/src/main/java/com/yapp/sharefood/healthcheck/controller/HealthCheckController.java
@@ -1,16 +1,21 @@
 package com.yapp.sharefood.healthcheck.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
+    private final Environment env;
+
     @ApiOperation("Application 생존 검사")
     @GetMapping("/healthcheck")
     public ResponseEntity<String> getHealthCheck() {
 
-        return ResponseEntity.ok("health check");
+        return ResponseEntity.ok("health check," + " Port(server.port) = " + env.getProperty("server.port"));
     }
 }
