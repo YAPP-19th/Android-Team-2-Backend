7504cf684e9046e3abb0f0cfbadfcc4167aa4b21
kihwankim <clack2933@gmail.com>
2021-09-23 19:47:53 +0900

    chore: oauth import하기 위해 application.yml에 include 추가

---
+++ module-external/src/test/java/com/yapp/sharefood/SharefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.sharefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharefoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
