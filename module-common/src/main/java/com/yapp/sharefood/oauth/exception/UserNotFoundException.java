d0ea3c73cf17da6209f102422910bd1c3b6a0998
kihwankim <clack2933@gmail.com>
2021-09-26 22:50:11 +0900

    feat: error msg 추가

--- module-common/src/main/java/com/yapp/sharefood/oauth/exception/UserNotFoundException.java
+++ module-common/src/main/java/com/yapp/sharefood/oauth/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.yapp.sharefood.oauth.exception;
 import com.yapp.sharefood.common.exception.NotFoundException;
 
 public class UserNotFoundException extends NotFoundException {
-    private static final String USER_NOT_FOUND_EXCEPTION_MSG = "";
+    private static final String USER_NOT_FOUND_EXCEPTION_MSG = "등록된 회원이 아닙니다.";
 
     public UserNotFoundException() {
         super(USER_NOT_FOUND_EXCEPTION_MSG);
