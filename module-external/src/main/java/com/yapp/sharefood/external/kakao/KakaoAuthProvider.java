f9bb27633ab6cbb88d49211484e43343d2e152e4
kihwankim <clack2933@gmail.com>
2021-10-01 21:14:48 +0900

    feat: kakao oauth 외부 주입 데이터 파일 분리

--- module-external/src/main/java/com/yapp/sharefood/external/kakao/KakaoAuthProvider.java
+++ module-external/src/main/java/com/yapp/sharefood/external/kakao/KakaoAuthProvider.java
@@ -5,7 +5,6 @@ import com.yapp.sharefood.external.OAuthProfile;
 import com.yapp.sharefood.external.exception.BadGatewayException;
 import com.yapp.sharefood.external.kakao.dto.KakaoOAuthProfile;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,14 +14,12 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class KakaoAuthProvider implements AuthProvider {
     private final WebClient webClient;
-
-    @Value("${oauth.kakao.profile-url}")
-    private String kakaoOAuthUrl;
+    private final KakaoOAuthComponent kakaoOAuthComponent;
 
     @Override
     public OAuthProfile getOAuthProfileInfo(String accessToken) {
         return webClient.post()
-                .uri(kakaoOAuthUrl)
+                .uri(kakaoOAuthComponent.oauthUrl())
                 .headers(headers -> headers.setBearerAuth(accessToken))
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new BadGatewayException(String.format("잘못된 kakao 토큰 (%s) 입니다", accessToken))))
