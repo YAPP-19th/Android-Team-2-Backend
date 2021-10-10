9885aa8aed771ed6be5d290df2f44f5472e98182
kihwankim <clack2933@gmail.com>
2021-10-11 01:42:51 +0900

    refactor: token 으로 얻은 user 정보 api 명세서 제거

--- module-api/src/main/java/com/yapp/sharefood/favor/controller/FavorController.java
+++ module-api/src/main/java/com/yapp/sharefood/favor/controller/FavorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 public class FavorController {
@@ -35,7 +36,7 @@ public class FavorController {
             @ApiResponse(code = 400, message = "[error] 입맛 정보를 입력하지 않은 경우와 입맛 정보가 없는 경우", response = HttpClientErrorException.BadRequest.class)
     })
     public ResponseEntity<Void> createUserFavor(@RequestBody MeFavorRequest meFavorRequest,
-                                                @AuthUser User user) {
+                                                @ApiIgnore @AuthUser User user) {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -44,7 +45,7 @@ public class FavorController {
     @ApiResponses({
             @ApiResponse(code = 200, message = "[success] 입 맛 정보 반환", response = FavorResponse.class)
     })
-    public ResponseEntity<FavorResponse> findUserFavors(@AuthUser User user) {
+    public ResponseEntity<FavorResponse> findUserFavors(@ApiIgnore @AuthUser User user) {
         FavorResponse favorResponse = new FavorResponse();
         return ResponseEntity.ok(favorResponse);
     }
