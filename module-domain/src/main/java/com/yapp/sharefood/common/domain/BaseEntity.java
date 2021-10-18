62c43986598ea035aa29cb4e0cbd78d227e95de2
kihwankim <clack2933@gmail.com>
2021-10-18 21:27:24 +0900

    fix: base entity 설계 이슈 수정

--- module-domain/src/main/java/com/yapp/sharefood/common/domain/BaseEntity.java
+++ module-domain/src/main/java/com/yapp/sharefood/common/domain/BaseEntity.java
@@ -9,11 +9,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity<LocalDateTime> {
+public abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createDate;
