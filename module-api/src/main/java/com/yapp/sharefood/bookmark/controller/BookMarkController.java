0aa78f2298b5a885bb3596139a872243b38192c9
DongHwan <ycdoit@naver.com>
2021-10-18 22:36:40 +0900

    feat: bookmark api 명세 operation 추가

--- module-api/src/main/java/com/yapp/sharefood/bookmark/controller/BookMarkController.java
+++ module-api/src/main/java/com/yapp/sharefood/bookmark/controller/BookMarkController.java
@@ -3,6 +3,7 @@ package com.yapp.sharefood.bookmark.controller;
 import com.yapp.sharefood.auth.resolver.AuthUser;
 import com.yapp.sharefood.bookmark.dto.BookMarkPageDto;
 import com.yapp.sharefood.user.domain.User;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
@@ -20,10 +21,12 @@ import java.net.URI;
 @RestController
 public class BookMarkController {
 
+    @ApiOperation("북마크 추가 API")
     @PostMapping("/api/v1/food/{foodId}/bookmark")
     @ApiResponses({
             @ApiResponse(code = 201, message = "[success] bookmark 추가 성공"),
-            @ApiResponse(code = 409, message = "[fail] 이미 추가된 bookmark가 존재합니다.", response = HttpClientErrorException.Conflict.class),
+            @ApiResponse(code = 404, message = "[error] 추가할 food를 찾을 수 없습니다.",  response = HttpClientErrorException.NotFound.class),
+            @ApiResponse(code = 409, message = "[error] 이미 추가된 bookmark가 존재합니다.", response = HttpClientErrorException.Conflict.class),
     })
     public ResponseEntity<Void> createBookMark(@ApiIgnore @AuthUser User user, @PathVariable("foodId") Long foodId) {
         Long id = 1L;
@@ -34,10 +37,11 @@ public class BookMarkController {
         return ResponseEntity.created(uri).build();
     }
 
+    @ApiOperation("북마크 삭제 API")
     @DeleteMapping("/api/v1/food/{foodId}/bookmark")
     @ApiResponses({
             @ApiResponse(code = 200, message = "[success] bookmark 삭제 성공"),
-            @ApiResponse(code = 404, message = "[fail] 삭제할 bookmark가 없습니다.", response = HttpClientErrorException.NotFound.class),
+            @ApiResponse(code = 404, message = "[error] 삭제할 bookmark가 없습니다.", response = HttpClientErrorException.NotFound.class),
     })
     public ResponseEntity<Void> deleteBookMark(@ApiIgnore @AuthUser User user, @PathVariable("foodId") Long foodId) {
         return ResponseEntity.ok().build();
