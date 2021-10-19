edea65c5c420157d79fb49595575c33e64023a99
kihwankim <clack2933@gmail.com>
2021-10-19 18:24:54 +0900

    feat: category entity 생성

---
+++ module-domain/src/main/java/com/yapp/sharefood/category/domain/Category.java
@@ -0,0 +1,29 @@
+package com.yapp.sharefood.category.domain;
+
+import com.yapp.sharefood.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> childCategories = new ArrayList<>();
+}
