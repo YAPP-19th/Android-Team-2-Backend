0859419da10f647da226c4cce1e08a1dec5e301e
kihwankim <clack2933@gmail.com>
2021-10-20 19:11:42 +0900

    feat: dept1 인 경우 category 정보 조회 기능

--- module-api/src/test/java/com/yapp/sharefood/category/service/CategoryServiceTest.java
+++ module-api/src/test/java/com/yapp/sharefood/category/service/CategoryServiceTest.java
@@ -1,17 +1,23 @@
 package com.yapp.sharefood.category.service;
 
+import com.yapp.sharefood.category.domain.Category;
 import com.yapp.sharefood.category.dto.response.CategoriesTreeResponse;
+import com.yapp.sharefood.category.repository.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class CategoryServiceTest {
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @Test
     @DisplayName("0개의 category")
@@ -29,20 +35,38 @@ class CategoryServiceTest {
     @DisplayName("dept 1, category 1개 인경우")
     void findAllTreeCategoryOneDataTest() throws Exception {
         // given
+        Category category = Category.of("category1");
+        categoryRepository.save(category);
 
         // when
+        CategoriesTreeResponse allTreeCategories = categoryService.findAllTreeCategories();
 
         // then
+        assertEquals(1, allTreeCategories.getCategories().size());
+        assertNull(allTreeCategories.getCategories().get(0).getChildCategories());
+        assertEquals("category1", allTreeCategories.getCategories().get(0).getCategoryName());
     }
 
     @Test
     @DisplayName("dept가 1인 category 여러개")
     void findAllCategoryOneDept() throws Exception {
         // given
+        Category category1 = Category.of("category1");
+        Category category2 = Category.of("category2");
+        Category category3 = Category.of("category3");
+        categoryRepository.save(category1);
+        categoryRepository.save(category2);
+        categoryRepository.save(category3);
 
         // when
+        CategoriesTreeResponse allTreeCategories = categoryService.findAllTreeCategories();
 
         // then
+        assertEquals(3, allTreeCategories.getCategories().size());
+        assertNull(allTreeCategories.getCategories().get(0).getChildCategories());
+        assertThat(allTreeCategories.getCategories())
+                .extracting("categoryName")
+                .contains("category1", "category2", "category3");
     }
 
     @Test
