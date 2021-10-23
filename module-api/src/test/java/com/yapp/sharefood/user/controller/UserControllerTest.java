414d68a634f32c21f04794720a42c89b3bc0b66f
kihwankim <clack2933@gmail.com>
2021-10-23 13:00:18 +0900

    test: nickname 관련 test code 추가

--- module-api/src/test/java/com/yapp/sharefood/user/controller/UserControllerTest.java
+++ module-api/src/test/java/com/yapp/sharefood/user/controller/UserControllerTest.java
@@ -2,40 +2,38 @@ package com.yapp.sharefood.user.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.yapp.sharefood.auth.token.TokenProvider;
+import com.yapp.sharefood.common.PreprocessController;
+import com.yapp.sharefood.user.dto.request.UserNicknameRequest;
 import com.yapp.sharefood.user.dto.response.UserNicknameResponse;
 import com.yapp.sharefood.user.exception.UserNicknameExistException;
-import com.yapp.sharefood.user.repository.UserRepository;
 import com.yapp.sharefood.user.service.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.willReturn;
-import static org.mockito.BDDMockito.willThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
-class UserControllerTest {
+class UserControllerTest extends PreprocessController {
     @Autowired
     MockMvc mockMvc;
     @MockBean
     UserService userService;
-
-    @MockBean
-    UserRepository userRepository;
-    @MockBean
-    TokenProvider tokenProvider;
 
     ObjectMapper objectMapper = new ObjectMapper();
 
@@ -58,10 +56,11 @@ class UserControllerTest {
                 new TypeReference<>() {
                 });
 
-        Assertions.assertEquals("냠냠박사 unique닉네임", userNicknameResponse.getNickname());
+        assertEquals("냠냠박사 unique닉네임", userNicknameResponse.getNickname());
     }
 
     @Test
+    @DisplayName("새로 생성된 user nickname이 이미 존재할 경우 예외 테스트")
     void findExistUserNicknameExceptionTest() throws Exception {
         // given
         willThrow(new UserNicknameExistException("해당 nickname은 이미 존재합니다."))
@@ -79,5 +78,138 @@ class UserControllerTest {
         assertThat(errMsg)
                 .isNotNull()
                 .isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("겹치는 nickname이 없을 경우")
+    void checkNicknameNotDuplicateTest() throws Exception {
+        // given
+        willDoNothing()
+                .given(userService).checkNicknameDuplicate(any(UserNicknameRequest.class));
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                get(String.format("/api/v1/users/%d/nickname/validation", authUserId))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .param("nickname", "newNickname"));
+
+        // then
+        perform.andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("자신의 nickname이 아닌 다른 사람의 닉네임 검사를 할 경우")
+    void checkNicknameNotMeTest() throws Exception {
+        // given
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                get(String.format("/api/v1/users/%d/nickname/validation", 2L))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .param("nickname", "newNickname"));
+
+        // then
+        perform.andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("nickname이 겹치는 경우")
+    void checkNicknameDuplicateTest() throws Exception {
+        // given
+        willThrow(new UserNicknameExistException())
+                .given(userService).checkNicknameDuplicate(any(UserNicknameRequest.class));
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                get(String.format("/api/v1/users/%d/nickname/validation", authUserId))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .param("nickname", "newNickname"));
+
+        // then
+        perform.andExpect(status().isConflict())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("user nickname 수정 테스트")
+    void userNicknameChangeTest() throws Exception {
+        // given
+        UserNicknameRequest request = new UserNicknameRequest("newNickname");
+        willReturn("newNickname")
+                .given(userService).changeUserNickname(anyLong(), any(UserNicknameRequest.class));
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch(String.format("/api/v1/users/%d/nickname", authUserId))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)));
+        // then
+        UserNicknameResponse response = objectMapper
+                .readValue(perform.andExpect(status().isOk())
+                        .andDo(print())
+                        .andReturn()
+                        .getResponse()
+                        .getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+                });
+
+        assertEquals("newNickname", response.getNickname());
+    }
+
+    @Test
+    @DisplayName("user nickname 수정 실패")
+    void userNicknameChangeFailTest() throws Exception {
+        // given
+        UserNicknameRequest request = new UserNicknameRequest("newNickname");
+        willThrow(UserNicknameExistException.class)
+                .given(userService).changeUserNickname(anyLong(), any(UserNicknameRequest.class));
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch(String.format("/api/v1/users/%d/nickname", authUserId))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        perform.andExpect(status().isConflict())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+    }
+
+    @Test
+    @DisplayName("권한 제한으로 인한 user nickname 수정 실패")
+    void userNicknameChangeFailCuzForbiddenTest() throws Exception {
+        // given
+        UserNicknameRequest request = new UserNicknameRequest("newNickname");
+        willReturn("newNickname")
+                .given(userService).changeUserNickname(anyLong(), any(UserNicknameRequest.class));
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                patch(String.format("/api/v1/users/%d/nickname", 2L))
+                        .header(HttpHeaders.AUTHORIZATION, "token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        perform.andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
     }
 }
