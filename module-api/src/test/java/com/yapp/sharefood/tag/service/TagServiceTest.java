6d1e074352e723e789ac713a05e773798437cf24
kihwankim <clack2933@gmail.com>
2021-10-28 21:01:42 +0900

    fix(test): service test를 위한 transactional 추가

--- module-api/src/test/java/com/yapp/sharefood/tag/service/TagServiceTest.java
+++ module-api/src/test/java/com/yapp/sharefood/tag/service/TagServiceTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class TagServiceTest {
 
     @Autowired
