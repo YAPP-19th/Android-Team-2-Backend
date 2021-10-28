894fe0afafb5c4b158667c9ed86c2f7ac62f3a50
kihwankim <clack2933@gmail.com>
2021-10-28 20:58:30 +0900

    refactor: tag parameter validate 설정

--- module-api/src/main/java/com/yapp/sharefood/tag/controller/TagController.java
+++ module-api/src/main/java/com/yapp/sharefood/tag/controller/TagController.java
@@ -1,12 +1,14 @@
 package com.yapp.sharefood.tag.controller;
 
+import com.yapp.sharefood.tag.dto.request.TagSearchRequest;
 import com.yapp.sharefood.tag.dto.response.TagSearchResponse;
 import com.yapp.sharefood.tag.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +17,7 @@ public class TagController {
     private final TagService tagService;
 
     @GetMapping("/api/v1/tags")
-    public ResponseEntity<TagSearchResponse> findSimilarTagNames(@RequestParam(name = "tagName") String tagName,
-                                                                 @RequestParam(name = "size") int size) {
-        return ResponseEntity.ok(tagService.findSimilarTags(tagName, size));
+    public ResponseEntity<TagSearchResponse> findSimilarTagNames(@Valid TagSearchRequest tagSearchRequest) {
+        return ResponseEntity.ok(tagService.findSimilarTags(tagSearchRequest.getTagName(), tagSearchRequest.getSize()));
     }
 }
