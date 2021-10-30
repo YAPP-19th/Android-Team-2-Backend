520bcf9a7f7c348480d40006d4e3a2be145f5aae
DongHwan <ycdoit@naver.com>
2021-10-30 14:21:11 +0900

    feat: request dto validation 조건 추가

--- module-api/src/main/java/com/yapp/sharefood/flavor/dto/request/UserFlavorRequest.java
+++ module-api/src/main/java/com/yapp/sharefood/flavor/dto/request/UserFlavorRequest.java
@@ -3,9 +3,11 @@ package com.yapp.sharefood.flavor.dto.request;
 import com.yapp.sharefood.flavor.dto.FlavorDto;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class UserFlavorRequest {
+    @NotNull
     private List<FlavorDto> flavors;
 }
