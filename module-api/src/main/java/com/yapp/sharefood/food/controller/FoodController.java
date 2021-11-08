b003717f778ee2f9e73704bd2feaaf1d97c60b63
kihwankim <clack2933@gmail.com>
2021-11-08 23:06:21 +0900

    feat: food request template 추가

--- module-api/src/main/java/com/yapp/sharefood/food/controller/FoodController.java
+++ module-api/src/main/java/com/yapp/sharefood/food/controller/FoodController.java
@@ -2,10 +2,15 @@ package com.yapp.sharefood.food.controller;
 
 import com.yapp.sharefood.auth.resolver.AuthUser;
 import com.yapp.sharefood.food.dto.request.FoodCreationRequest;
+import com.yapp.sharefood.food.dto.request.FoodPageSearchRequest;
 import com.yapp.sharefood.food.dto.response.FoodDetailResponse;
+import com.yapp.sharefood.food.dto.response.FoodPageResponse;
 import com.yapp.sharefood.food.service.FoodService;
 import com.yapp.sharefood.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,5 +36,11 @@ public class FoodController {
     @GetMapping("/api/v1/foods/{foodId}")
     public ResponseEntity<FoodDetailResponse> findFood(@PathVariable("foodId") Long foodId) {
         return ResponseEntity.ok(foodService.findFoodById(foodId));
+    }
+
+    @GetMapping("/api/v1/foods")
+    public ResponseEntity<FoodPageResponse> findFoodPageData(FoodPageSearchRequest foodPageSearchRequest,
+                                                             @PageableDefault(sort = "createDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        return null;
     }
 }
