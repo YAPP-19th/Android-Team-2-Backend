5df4f314ebb8a8a5fc143ffdb051cddee785ed6c
dev-donghwan <ycdoit@naver.com>
2021-11-12 23:37:32 +0900

    feat: flavor controller advice 생성

---
+++ module-api/src/main/java/com/yapp/sharefood/flavor/advice/FlavorAdviceController.java
@@ -0,0 +1,21 @@
+package com.yapp.sharefood.flavor.advice;
+
+import com.yapp.sharefood.flavor.exception.FlavorNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class FlavorAdviceController {
+
+    @ExceptionHandler(FlavorNotFoundException.class)
+    protected ResponseEntity<Object> handleFlavorNotFoundException(final FlavorNotFoundException exception) {
+        log.error(exception.getMessage(), exception);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+}
