1e8631fb73b84ebd6c132719f38e43cd57dc9592
kihwankim <clack2933@gmail.com>
2021-11-17 17:42:47 +0900

    refactor: main type의 재료가 없을 경우 에러 띄우발생 기능 추가

--- module-common/src/main/java/com/yapp/sharefood/common/exception/file/FileTypeValidationException.java
+++ module-common/src/main/java/com/yapp/sharefood/common/exception/file/FileTypeValidationException.java
@@ -1,6 +1,8 @@
 package com.yapp.sharefood.common.exception.file;
 
-public class FileTypeValidationException extends RuntimeException {
+import com.yapp.sharefood.common.exception.BadRequestException;
+
+public class FileTypeValidationException extends BadRequestException {
     private static final String FILE_TYPE_VALIDATION_EXCEPTION = "입력된 파일 확장자가 적절하지 않습니다.";
 
     public FileTypeValidationException() {
