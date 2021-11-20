8fc5b3c143eabdb77ccaf121a1339f7d127b4e20
DongHwan <ycdoit@naver.com>
2021-11-20 18:13:55 +0900

    feat: advice controller 추가

---
+++ module-api/src/main/java/com/yapp/sharefood/bookmark/advice/BookmarkAdviceController.java
@@ -0,0 +1,19 @@
+package com.yapp.sharefood.bookmark.advice;
+
+import com.yapp.sharefood.bookmark.exception.BookmarkNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class BookmarkAdviceController {
+
+    @ExceptionHandler(BookmarkNotFoundException.class)
+    protected ResponseEntity<Object> handleBookmarkNotFoundException(final BookmarkNotFoundException exception) {
+        log.error(exception.getMessage(), exception);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+}
