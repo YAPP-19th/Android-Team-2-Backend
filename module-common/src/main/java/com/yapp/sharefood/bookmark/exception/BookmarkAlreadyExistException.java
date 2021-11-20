eb9f54143f6a1dd986c10723e284b3b02d21745d
DongHwan <ycdoit@naver.com>
2021-11-21 00:59:03 +0900

    feat: exception message private to public

--- module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkAlreadyExistException.java
+++ module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkAlreadyExistException.java
@@ -3,7 +3,7 @@ package com.yapp.sharefood.bookmark.exception;
 import com.yapp.sharefood.common.exception.ConflictException;
 
 public class BookmarkAlreadyExistException extends ConflictException {
-    private static final String BOOKMARK_ALREADY_EXIST_MSG = "북마크가 이미 존재합니다.";
+    public static final String BOOKMARK_ALREADY_EXIST_MSG = "북마크가 이미 존재합니다.";
 
     public BookmarkAlreadyExistException() {
         super(BOOKMARK_ALREADY_EXIST_MSG);
--- module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkNotFoundException.java
+++ module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkNotFoundException.java
@@ -3,9 +3,13 @@ package com.yapp.sharefood.bookmark.exception;
 import com.yapp.sharefood.common.exception.NotFoundException;
 
 public class BookmarkNotFoundException extends NotFoundException {
-    private static final String BOOKMARK_NOT_FOUND_EXCEPTION_MSG = "bookmark를 찾지 못 하였습니다.";
+    public static final String BOOKMARK_NOT_FOUND_EXCEPTION_MSG = "북마크를 찾지 못하였습니다.";
 
     public BookmarkNotFoundException() {
         super(BOOKMARK_NOT_FOUND_EXCEPTION_MSG);
+    }
+
+    public BookmarkNotFoundException(String msg) {
+        super(msg);
     }
 }
