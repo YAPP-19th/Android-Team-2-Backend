b735764f799897bc79afd208adab3da6e432efce
DongHwan <ycdoit@naver.com>
2021-11-20 22:58:02 +0900

    refactor: 오타 수정

--- module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkNotFoundException.java
+++ module-common/src/main/java/com/yapp/sharefood/bookmark/exception/BookmarkNotFoundException.java
@@ -3,9 +3,9 @@ package com.yapp.sharefood.bookmark.exception;
 import com.yapp.sharefood.common.exception.NotFoundException;
 
 public class BookmarkNotFoundException extends NotFoundException {
-    private static final String LIKE_NOT_FOUND_EXCEPTION_MSG = "bookmark를 찾지 못 하였습니다.";
+    private static final String BOOKMARK_NOT_FOUND_EXCEPTION_MSG = "bookmark를 찾지 못 하였습니다.";
 
     public BookmarkNotFoundException() {
-        super(LIKE_NOT_FOUND_EXCEPTION_MSG);
+        super(BOOKMARK_NOT_FOUND_EXCEPTION_MSG);
     }
 }
