bb41e424195aadc33f1c17899d4ebb3078d3c64c
DongHwan <ycdoit@naver.com>
2021-11-20 18:10:42 +0900

    feat: bookmark domain 추가

---
+++ module-domain/src/main/java/com/yapp/sharefood/bookmark/Bookmark.java
@@ -0,0 +1,53 @@
+package com.yapp.sharefood.bookmark;
+
+import com.yapp.sharefood.common.domain.BaseEntity;
+import com.yapp.sharefood.common.exception.InvalidOperationException;
+import com.yapp.sharefood.food.domain.Food;
+import com.yapp.sharefood.food.exception.FoodNotFoundException;
+import com.yapp.sharefood.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"food_id", "user_id"}))
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmark extends BaseEntity {
+
+    @Id
+    @Column(name = "bookmark_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Bookmark(User user) {
+        this.user = user;
+    }
+
+    public Bookmark of(User user) {
+        return new Bookmark(user);
+    }
+
+    public void assignFood(Food food) {
+        if (Objects.isNull(food)) {
+            throw new FoodNotFoundException();
+        }
+
+        if (Objects.nonNull(this.food)) {
+            throw new InvalidOperationException("한번 할당된 게시글은 변경할 수 없습니다.");
+        }
+
+        this.food = food;
+    }
+}
---
+++ module-domain/src/main/java/com/yapp/sharefood/food/domain/Bookmarks.java
@@ -0,0 +1,64 @@
+package com.yapp.sharefood.food.domain;
+
+import com.yapp.sharefood.bookmark.Bookmark;
+import com.yapp.sharefood.bookmark.exception.BookmarkNotFoundException;
+import com.yapp.sharefood.common.exception.InvalidOperationException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bookmarks {
+
+    @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Bookmark> bookmarks = new ArrayList<>();
+
+    public int getSize() {
+        return bookmarks.size();
+    }
+
+    public void assignBookmark(Food food, Bookmark bookmark) {
+        if (Objects.isNull(bookmark)) {
+            throw new BookmarkNotFoundException();
+        }
+
+        validateAlreadyBookmarkUser(bookmark.getId());
+
+        bookmark.assignFood(food);
+        if(!bookmarks.contains(bookmark)) {
+            bookmarks.add(bookmark);
+        }
+    }
+
+    public void deleteBookmark(Long userId) {
+        Bookmark bookmark = findBookmarkByUserId(userId);
+        bookmarks.remove(bookmark);
+    }
+
+    private Bookmark findBookmarkByUserId(Long userId) {
+        return this.bookmarks.stream()
+                .filter(bookmark -> bookmark.getUser().getId().equals(userId))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private void validateAlreadyBookmarkUser(Long userId) {
+        if (isAlreadyBookmark(userId)) {
+            throw new InvalidOperationException("이미 bookmark한 사용자 입니다.");
+        }
+    }
+
+    private boolean isAlreadyBookmark(Long userId) {
+        return bookmarks.stream()
+                .anyMatch(bookmark -> bookmark.getUser().getId().equals(userId));
+    }
+}
