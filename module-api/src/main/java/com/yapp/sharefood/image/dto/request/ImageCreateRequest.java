6bfbafc525ee7de96dfbc5d5546d1f28d48e02af
kihwankim <clack2933@gmail.com>
2021-11-23 21:34:01 +0900

    fix: array size max 값 설정

--- module-api/src/main/java/com/yapp/sharefood/image/dto/request/ImageCreateRequest.java
+++ module-api/src/main/java/com/yapp/sharefood/image/dto/request/ImageCreateRequest.java
@@ -3,13 +3,13 @@ package com.yapp.sharefood.image.dto.request;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class ImageCreateRequest {
-    @Max(3)
+    @Size(max = 3)
     @NotNull
     private List<MultipartFile> images;
 }
