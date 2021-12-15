f6bea6a17bbe7d38d806d71fed63db52bc5c4f55
kihwankim <clack2933@gmail.com>
2021-12-15 19:39:42 +0900

    test: flavor update 기능 테스트 코드 추가

--- module-domain/src/test/java/com/yapp/sharefood/food/domain/FoodFlavorsTest.java
+++ module-domain/src/test/java/com/yapp/sharefood/food/domain/FoodFlavorsTest.java
@@ -4,12 +4,16 @@ import com.yapp.sharefood.common.mockdomain.MockFood;
 import com.yapp.sharefood.flavor.domain.Flavor;
 import com.yapp.sharefood.flavor.domain.FlavorType;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FoodFlavorsTest {
     static Map<Long, Flavor> flavorDb = new HashMap<>();
@@ -25,20 +29,51 @@ class FoodFlavorsTest {
 
 
     @Test
+    @DisplayName("flavor update 기능 테스트 - 성공")
     void updateFlavorsTest_Success() {
         // given
         Food mockFood = new MockFood();
-        List<FoodFlavor> foodFlavors = new ArrayList<>();
-//        foodFlavors.add(new FoodFlavor(mockFood, ));
-//        foodFlavors.add(new FoodFlavor(mockFood, ));
-//        foodFlavors.add(new FoodFlavor(mockFood, ));
-//        foodFlavors.add(new FoodFlavor(mockFood, ));
-//        foodFlavors.add(new FoodFlavor(mockFood, ));
+        mockFood.assignFlavors(List.of(flavorDb.get(1L), flavorDb.get(2L), flavorDb.get(3L)));
+
+        List<Flavor> changeFlavors = new ArrayList<>();
+        changeFlavors.add(flavorDb.get(3L));
+        changeFlavors.add(flavorDb.get(4L));
+        changeFlavors.add(flavorDb.get(5L));
 
         // when
-        List<Flavor> changeFlavors = new ArrayList<>();
-//        changeFlavors.add()
+        mockFood.getFoodFlavors()
+                .updateFlavors(changeFlavors, mockFood);
 
         // then
+        assertThat(mockFood.getFoodFlavors().getFoodFlavors())
+                .isNotNull()
+                .hasSize(3);
+
+        List<FlavorType> assignedFlavorTypes = mockFood.getFoodFlavors().getFoodFlavors().stream()
+                .map(foodFlavor -> foodFlavor.getFlavor().getFlavorType())
+                .collect(Collectors.toList());
+        assertThat(assignedFlavorTypes)
+                .isNotNull()
+                .hasSize(3)
+                .containsExactlyInAnyOrderElementsOf(List.of(FlavorType.SALTY, FlavorType.BITTER, FlavorType.SOUR));
+    }
+
+    @Test
+    @DisplayName("flavor update 빈 값인 경우 - 성공")
+    void updateFlavorsEmptyListTest_Success() {
+        // given
+        Food mockFood = new MockFood();
+        mockFood.assignFlavors(List.of(flavorDb.get(1L), flavorDb.get(2L), flavorDb.get(3L)));
+
+        List<Flavor> changeFlavors = new ArrayList<>();
+
+        // when
+        mockFood.getFoodFlavors()
+                .updateFlavors(changeFlavors, mockFood);
+
+        // then
+        assertThat(mockFood.getFoodFlavors().getFoodFlavors())
+                .isNotNull()
+                .hasSize(0);
     }
 }
