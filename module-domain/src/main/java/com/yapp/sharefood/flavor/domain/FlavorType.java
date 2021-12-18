a5737af8a7b29a3e9a2dc7f4c13daac8e1a02d6e
kihwankim <clack2933@gmail.com>
2021-12-18 21:51:36 +0900

    refactor: 오타 수정

--- module-domain/src/main/java/com/yapp/sharefood/flavor/domain/FlavorType.java
+++ module-domain/src/main/java/com/yapp/sharefood/flavor/domain/FlavorType.java
@@ -7,9 +7,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum FlavorType {
-    SWEET("단맛"), SPICY("매운맛"), SALTY("짠맛"), BITTER("쓴맛"), SOUR("씬맛"),
-    SWEET_DETAIL("달콤함"), PLAIN_DETAIL("단백한"), SOUR_DETAIL("새콤한"), COOL_DETAIL("시원한"), SALTY_DETAIL("짭짤한"), GREASY_DETAIL("느끼한"),
-    ODD_DETAIL("삼삼함"), TOO_SPICY_DETAIL("매콤한"), SPICY_DETAIL("얼큰한"), CLEAR_DETAIL("깔끔한"), SAVORY_DETAIL("고소한"), REFRESH_DETAIL("개운한");
+    SWEET("단맛"), SPICY("매운맛"), SALTY("짠맛"), BITTER("쓴맛"), SOUR("신맛"),
+    SWEET_DETAIL("달콤한"), PLAIN_DETAIL("담백한"), SOUR_DETAIL("새콤한"), COOL_DETAIL("시원한"), SALTY_DETAIL("짭짤한"), GREASY_DETAIL("느끼한"),
+    ODD_DETAIL("삼삼한"), TOO_SPICY_DETAIL("매콤한"), SPICY_DETAIL("얼큰한"), CLEAR_DETAIL("깔끔한"), SAVORY_DETAIL("고소한"), REFRESH_DETAIL("개운한");
 
     private final String flavorName;
 
