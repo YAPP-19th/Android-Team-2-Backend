b9f5d758aa2e9c7ecb7367b721e9841e0a55f56c
kihwankim <clack2933@gmail.com>
2021-12-26 18:44:53 +0900

    fix: covering index로 발생하는 order by 이슈 해결

--- module-domain/src/main/java/com/yapp/sharefood/food/repository/query/FoodQueryRepository.java
+++ module-domain/src/main/java/com/yapp/sharefood/food/repository/query/FoodQueryRepository.java
@@ -1,15 +1,19 @@
 package com.yapp.sharefood.food.repository.query;
 
+import com.yapp.sharefood.common.order.SortType;
 import com.yapp.sharefood.food.domain.Food;
 import com.yapp.sharefood.food.dto.FoodMinePageSearch;
 import com.yapp.sharefood.food.dto.FoodPageSearch;
 import com.yapp.sharefood.food.dto.FoodRecommendSearch;
+import com.yapp.sharefood.food.dto.OrderType;
 import com.yapp.sharefood.user.domain.User;
 
 import java.util.List;
 
 public interface FoodQueryRepository {
     List<Food> findFoodWithCategoryByIds(List<Long> ids);
+
+    List<Food> findFoodWithCategoryByIds(List<Long> ids, SortType sortType, OrderType orderType);
 
     List<Food> findFavoriteFoods(User findUser);
 
