1cdb54eaecb7d0a89167fb4acd6311d390e17c16
kihwankim <clack2933@gmail.com>
2022-01-04 20:24:08 +0900

    feat: flavor data 마이그레이션 코드 추가

---
+++ module-domain/src/main/resources/db/migration/V5__flavor_data.sql
@@ -0,0 +1,34 @@
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (1, now(), now(), 'SWEET');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (2, now(), now(), 'SPICY');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (3, now(), now(), 'SALTY');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (4, now(), now(), 'BITTER');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (5, now(), now(), 'SOUR');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (6, now(), now(), 'SWEET_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (7, now(), now(), 'PLAIN_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (8, now(), now(), 'SOUR_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (9, now(), now(), 'COOL_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (10, now(), now(), 'SALTY_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (11, now(), now(), 'GREASY_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (12, now(), now(), 'ODD_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (13, now(), now(), 'TOO_SPICY_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (14, now(), now(), 'SPICY_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (15, now(), now(), 'CLEAR_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (16, now(), now(), 'SAVORY_DETAIL');
+INSERT INTO flavor (flavor_id, create_date, last_modified_date, flavor_type)
+VALUES (17, now(), now(), 'REFRESH_DETAIL');
