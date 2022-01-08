0eb92140fd3ffde746ea626615f3a497b361c427
kihwankim <clack2933@gmail.com>
2022-01-08 20:01:54 +0900

    chore: prod 설정 deploy shall script 추가

---
+++ script/real-deploy.sh
@@ -0,0 +1,19 @@
+BUILD_JAR=$(ls /root/shared-food/shared-food/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >>/root/shared-food/deploy.log
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >>/root/shared-food/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+
+echo "> DEPLOY_JAR 배포" >>/root/shared-food/deploy.log
+
+nohup java -jar $DEPLOY_JAR --spring.profiles.active=prod 2>&1 &
