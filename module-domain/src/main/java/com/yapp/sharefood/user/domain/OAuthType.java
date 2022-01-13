961e4d241323a22a168cfe4ae62af3950dbfaf68
kihwankim <clack2933@gmail.com>
2022-01-13 21:17:52 +0900

    refactor: 사용하지 않는 편의 메서드 제거

--- module-domain/src/main/java/com/yapp/sharefood/user/domain/OAuthType.java
+++ module-domain/src/main/java/com/yapp/sharefood/user/domain/OAuthType.java
@@ -1,9 +1,6 @@
 package com.yapp.sharefood.user.domain;
 
-import com.yapp.sharefood.oauth.exception.OAuthTypeNotFoundException;
 import lombok.Getter;
-
-import java.util.Arrays;
 
 @Getter
 public enum OAuthType {
@@ -13,12 +10,5 @@ public enum OAuthType {
 
     OAuthType(String type) {
         this.oAuthProviderName = type;
-    }
-
-    public static OAuthType findByTypeName(String oAuthTypeName) {
-        return Arrays.stream(OAuthType.values())
-                .filter(oauth -> oauth.name().equals(oAuthTypeName.toUpperCase()))
-                .findFirst()
-                .orElseThrow(OAuthTypeNotFoundException::new);
     }
 }
