81dd5e8e5b0621d499a66a65b3824695ca24900e
kihwankim <clack2933@gmail.com>
2022-01-31 14:40:13 +0900

    refoctor: category advice contoller 설정

--- module-api/src/main/java/com/yapp/sharefood/category/advice/CategoryAdviceController.java
+++ module-api/src/main/java/com/yapp/sharefood/category/advice/CategoryAdviceController.java
@@ -1,8 +1,10 @@
 package com.yapp.sharefood.category.advice;
 
 import com.yapp.sharefood.category.exception.CategoryNotFoundException;
-import lombok.RequiredArgsConstructor;
+import com.yapp.sharefood.common.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CategoryAdviceController {
     @ExceptionHandler(CategoryNotFoundException.class)
-    protected ResponseEntity<String> handlerCategoryNotFoundException(final CategoryNotFoundException exception) {
+    protected ResponseEntity<ErrorResponse> handlerCategoryNotFoundException(final CategoryNotFoundException exception) {
         log.info("CategoryNotFoundException: {}", exception.getMessage(), exception);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 }
